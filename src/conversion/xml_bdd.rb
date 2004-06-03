3830aec80707de2f18511821a3749c629b691c7b
hwyss <hwyss@gentoo.hwyss.net>
2004-06-03 08:41:07 +0200

    ChangeSet 1.4.1.2: UserStories_XmlConv2.txt: ChangeSet 1.4.1.2:   Rename: user-stories/UserStories_Janico.txt -> user-stories/UserStories_XmlConv2.txt ChangeSet 1.4.1.2: UserStories_Janico.txt: ChangeSet 1.4.1.2:   Questions about the Order-UserStory ChangeSet 1.4.1.2: xml_bdd.rb: ChangeSet 1.4.1.2:   Rename: src/conversion/bdd_factory.rb -> src/conversion/xml_bdd.rb ChangeSet 1.4.1.2:   Rename: test/test_conversion/bdd_factory.rb -> test/test_conversion/xml_bdd.rb ChangeSet 1.4.1.2: bdd_factory.rb: ChangeSet 1.4.1.2:   new file ChangeSet 1.4.1.2: party.rb: ChangeSet 1.4.1.2:   Rename: test/test_party.rb -> test/test_model/party.rb ChangeSet 1.4.1.2: name.rb: ChangeSet 1.4.1.2:   Rename: test/test_name.rb -> test/test_model/name.rb ChangeSet 1.4.1.2: delivery.rb: ChangeSet 1.4.1.2:   Rename: test/test_delivery.rb -> test/test_model/delivery.rb ChangeSet 1.4.1.2: delivery_item.rb: ChangeSet 1.4.1.2:   Rename: test/test_delivery_item.rb -> test/test_model/delivery_item.rb ChangeSet 1.4.1.2: address.rb: ChangeSet 1.4.1.2:   Rename: test/test_address.rb -> test/test_model/address.rb test/test_address.rb: Rename: test/test_address.rb -> test/test_model/address.rb test/test_delivery_item.rb: Rename: test/test_delivery_item.rb -> test/test_model/delivery_item.rb test/test_delivery.rb: Rename: test/test_delivery.rb -> test/test_model/delivery.rb test/test_name.rb: Rename: test/test_name.rb -> test/test_model/name.rb test/test_party.rb: Rename: test/test_party.rb -> test/test_model/party.rb test/test_conversion/bdd_factory.rb: Rename: test/test_conversion/bdd_factory.rb -> test/test_conversion/xml_bdd.rb src/conversion/bdd_factory.rb: Rename: src/conversion/bdd_factory.rb -> src/conversion/xml_bdd.rb user-stories/UserStories_Janico.txt: Rename: user-stories/UserStories_Janico.txt -> user-stories/UserStories_XmlConv2.txt user-stories/UserStories_Janico.txt: Questions about the Order-UserStory

---
+++ src/conversion/xml_bdd.rb
@@ -0,0 +1,120 @@
+#!/usr/bin/env ruby
+# BddFactory -- xmlconv2 -- 01.06.2004 -- hwyss@ywesee.com
+
+require 'date'
+require 'rexml/document'
+require 'rexml/xpath'
+require 'model/address'
+require 'model/bdd'
+require 'model/bsr'
+require 'model/delivery'
+require 'model/delivery_item'
+require 'model/name'
+require 'model/party'
+
+module XmlConv
+	module Conversion
+		class BddFactory
+			class << self
+				def xml2bdd(xml_document)
+					bdd = Model::Bdd.new
+					if(xml_bsr = REXML::XPath.first(xml_document, 'BDD/BSR'))
+						_bdd_add_xml_bsr(bdd, xml_bsr)
+					end
+					REXML::XPath.each(xml_document, 'BDD/Delivery') { |xml_delivery|
+						_bdd_add_xml_delivery(bdd, xml_delivery)
+					}
+					bdd
+				end
+				def _bdd_add_xml_bsr(bdd, xml_bsr)
+					bsr = Model::Bsr.new
+					REXML::XPath.each(xml_bsr, 'Party') { |xml_party|
+						_container_add_xml_party(bsr, xml_party)
+					}
+					bdd.bsr = bsr
+				end
+				def _bdd_add_xml_delivery(bdd, xml_delivery)
+					delivery = Model::Delivery.new
+					REXML::XPath.each(xml_delivery, 'DeliveryId') { |xml_id|
+						_container_add_xml_id(delivery, xml_id)
+					}
+					REXML::XPath.each(xml_delivery, 'Party') { |xml_party| 
+						_container_add_xml_party(delivery, xml_party)
+					}
+					REXML::XPath.each(xml_delivery, 'DeliveryItem') { |xml_item|
+						_delivery_add_xml_item(delivery, xml_item)	
+					}
+					bdd.add_delivery(delivery)
+				end
+				def _container_add_xml_party(container, xml_party)
+					party = Model::Party.new
+					party.role = _latin1(xml_party.attribute('Role').value)
+					REXML::XPath.each(xml_party, 'PartyId') { |xml_id|
+						_container_add_xml_id(party, xml_id)
+					}
+					if(xml_name = REXML::XPath.first(xml_party, 'Name'))
+						_party_add_xml_name(party, xml_name)
+					end
+					if(xml_address = REXML::XPath.first(xml_party, 'Address'))
+						_party_add_xml_address(party, xml_address)
+					end
+					REXML::XPath.each(xml_party, 'Party') { |xml_inner_party|
+						_container_add_xml_party(party, xml_inner_party)
+					}
+					container.add_party(party)
+				end
+				def _container_add_xml_id(container, xml_id)
+					domain = _latin1(xml_id.attribute('Domain').value)
+					value = _latin1(xml_id.text)
+					container.add_id(domain, value)
+				end
+				def _delivery_add_xml_item(delivery, xml_item)
+					item = Model::DeliveryItem.new
+					xml_line_no = REXML::XPath.first(xml_item, 'LineNo')
+					item.line_no = _latin1(xml_line_no.text)
+					REXML::XPath.each(xml_item, 'PartId/IdentNo') { |xml_id| 
+						_container_add_xml_id(item, xml_id)	
+					}
+					xml_qty = REXML::XPath.first(xml_item, 'Qty')
+					item.qty = xml_qty.text.to_i
+					if(xml_date = REXML::XPath.first(xml_item, 'DeliveryDate'))
+						raw = _latin1(xml_date.text)
+						item.delivery_date = begin
+							Date.parse(raw.gsub(/\./, '-'))
+						rescue
+							Date.today
+						end
+					end
+					delivery.add_item(item)
+				end
+				def _party_add_xml_address(party, xml_address)
+					address = Model::Address.new
+					REXML::XPath.each(xml_address, 'AddressLine') { |xml_addr_line|
+						address.add_line(_latin1(xml_addr_line.text))
+					}
+					xml_city = REXML::XPath.first(xml_address, 'City')
+					address.city = _latin1(xml_city.text)
+					xml_zip_code = REXML::XPath.first(xml_address, 'ZipCode')
+					address.zip_code = _latin1(xml_zip_code.text)
+					party.address = address
+				end
+				def _party_add_xml_name(party, xml_name)
+					name = Model::Name.new
+					name.text = _latin1(xml_name.text)
+					if(xml_first = REXML::XPath.first(xml_name, 'FirstName'))
+						name.first = _latin1(xml_first.text)
+					end
+					if(xml_last = REXML::XPath.first(xml_name, 'LastName'))
+						name.last = _latin1(xml_last.text)
+					end
+					party.name = name
+				end
+				def _latin1(str)
+					Iconv.iconv('ISO-8859-1', 'UTF8', str).first.strip
+				rescue
+					str
+				end
+			end
+		end
+	end
+end
