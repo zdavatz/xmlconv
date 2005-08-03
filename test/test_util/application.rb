zdavatz/xmlconv

commit 0d213f4d6baf81f9120f200037ec4a24ec1aa5d8

Gracefully survive failure of sending a notification email. fixes http://ydpm.ywesee.com/xmlconv2/lost_in_translation

src/util/application.rb
@@ -34,12 +34,12 @@ module XmlConv
 			def execute(transaction)
 				transaction.transaction_id = next_transaction_id
 				transaction.execute
+				transaction.notify
 			rescue Exception => error
 				transaction.error = error
 			ensure
-				transaction.notify
 				@transactions.push(transaction)
-				ODBA.batch {
+				ODBA.transaction {
 					@transactions.odba_store
 				}
 			end

test/test_util/application.rb
@@ -2,6 +2,7 @@
 # XmlConv::TestApplication -- xmlconv2 -- 07.06.2004 -- hwyss@ywesee.com
 
 $: << File.dirname(__FILE__)
+$: << File.expand_path('..', File.dirname(__FILE__))
 $: << File.expand_path('../../src', File.dirname(__FILE__))
 
 require 'test/unit'
@@ -46,6 +47,36 @@ module XmlConv
 				}
 				transaction.__next(:execute) { }
 				transaction.__next(:notify) { }
+				assert_equal([], @app.transactions)
+				assert_equal(0, @app.transactions.size)
+				@app.execute(transaction)
+				assert_equal([transaction], @app.transactions)
+				transaction.__verify
+				cache.__verify
+			ensure
+				ODBA.storage = nil
+				ODBA.cache_server = nil
+			end
+			def test_execute__survive_notification_failure
+				storage = Mock.new('Storage')
+				storage.__next(:transaction) { |block|
+					block.call
+				}
+				ODBA.storage = storage
+				transaction = Mock.new('Transaction')
+				cache = Mock.new('Cache')
+				cache.__next(:store) { |transactions|
+					assert_equal(@app.transactions, transactions)
+				}
+				ODBA.cache_server = cache
+				transaction.__next(:transaction_id=) { |id|
+					assert_equal(1, id)
+				}
+				transaction.__next(:execute) { }
+				transaction.__next(:notify) { 
+					raise Net::SMTPFatalError, 'could not send email'
+				}
+				transaction.__next(:error=) { }
 				assert_equal([], @app.transactions)
 				assert_equal(0, @app.transactions.size)
 				@app.execute(transaction)

test/test_util/destination.rb
@@ -2,6 +2,7 @@
 # TestDestination -- xmlconv2 -- 08.06.2004 -- hwyss@ywesee.com
 
 $: << File.dirname(__FILE__)
+$: << File.expand_path('..', File.dirname(__FILE__))
 $: << File.expand_path('../../src', File.dirname(__FILE__))
 
 require 'test/unit'
@@ -169,7 +170,7 @@ module XmlConv
 				http_session.__next(:request) { |post_request, body| 
 					assert_instance_of(Net::HTTP::Post, post_request)
 					header = post_request.instance_variable_get('@header') 
-					assert_equal('text/xml', header['content-type'])
+					assert_equal(['text/xml'], header['content-type'])
 					assert(header.include?('authorization'), "Authorization-Headers not sent")
 					assert_equal('The Delivery', body)
 					response

commit 608f49dd4eb35c04afe9761e45946bb8b9cdbdb8

fix bug that was introduced with commit 0d213f4d6baf81f9120f200037ec4a24ec1aa5d8, where notifications were only sent in successful transactions but not if an error occurred...

src/util/application.rb
@@ -32,9 +32,14 @@ module XmlConv
 				transaction.destination = destination
 =end
 			def execute(transaction)
+				_execute(transaction)
+				transaction.notify
+			rescue Exception => error
+				## survive notification failure
+			end
+			def _execute(transaction)
 				transaction.transaction_id = next_transaction_id
 				transaction.execute
-				transaction.notify
 			rescue Exception => error
 				transaction.error = error
 			ensure

test/test_util/application.rb
@@ -76,7 +76,36 @@ module XmlConv
 				transaction.__next(:notify) { 
 					raise Net::SMTPFatalError, 'could not send email'
 				}
+				assert_equal([], @app.transactions)
+				assert_equal(0, @app.transactions.size)
+				@app.execute(transaction)
+				assert_equal([transaction], @app.transactions)
+				transaction.__verify
+				cache.__verify
+			ensure
+				ODBA.storage = nil
+				ODBA.cache_server = nil
+			end
+			def test_execute__notify_errors
+				storage = Mock.new('Storage')
+				storage.__next(:transaction) { |block|
+					block.call
+				}
+				ODBA.storage = storage
+				transaction = Mock.new('Transaction')
+				cache = Mock.new('Cache')
+				cache.__next(:store) { |transactions|
+					assert_equal(@app.transactions, transactions)
+				}
+				ODBA.cache_server = cache
+				transaction.__next(:transaction_id=) { |id|
+					assert_equal(1, id)
+				}
+				transaction.__next(:execute) {
+					raise 'oops, something went wrong'
+				}
 				transaction.__next(:error=) { }
+				transaction.__next(:notify) { }
 				assert_equal([], @app.transactions)
 				assert_equal(0, @app.transactions.size)
 				@app.execute(transaction)