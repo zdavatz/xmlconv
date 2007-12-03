5be29bf4fbffe540b167589e9242ec133cd693b4
Hannes Wyss <hwyss@ywesee.com>
2007-12-03 18:14:00 +0100

    Added Missing File

---
+++ lib/xmlconv/model/document.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# Model::Document -- xmlconv -- 03.12.2007 -- hwyss@ywesee.com
+
+module XmlConv
+  module Model
+    class Document < String
+			attr_accessor :recipient_id, :filename, :prefix, :transaction_id
+      def initialize(*args)
+        super
+				@recipient_id = recipient_id
+        @prefix = @recipient_id
+        time = Time.now
+				msec = sprintf('%03i', (time.to_f * 1000).to_i % 100)
+        @transaction_id = time.strftime("%Y%m%d%H%M%S#{msec}")
+      end
+      def filename
+        @filename || sprintf("%s%s_%s.dat", @prefix, @suffix, @transaction_id)
+      end
+      def to_s
+        "" << self
+      end
+    end
+	end
+end
