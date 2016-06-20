4ac1af64b4868fb6846a89512dd7ff9ec2bbd17d
Niklaus Giger <ngiger@ywesee.com>
2016-06-20 12:30:31 +0200

    Using config options for mail from config.yml given

--- lib/xmlconv/util/mail.rb
+++ lib/xmlconv/util/mail.rb
@@ -10,13 +10,30 @@ module XmlConv
 module Mail
   SMTP_HANDLER = Net::SMTP
   def Mail.notify recipients, my_subject, my_body
+    config = XmlConv::CONFIG
+    if config.mail_suppress_sending
+      puts "#{__FILE__}:#{__LINE__} Suppress sending mail with subject: #{my_subject}"
+      ::Mail.defaults do  delivery_method :test end
+    else
+      puts "Mail.sendmail #{config.smtp_server} #{config.smtp_port} smtp_user: #{config.smtp_user}"
+      ::Mail.defaults do
+      options = { :address              => config.smtp_server,
+                  :port                 => config.smtp_port,
+                  :domain               => config.smtp_domain,
+                  :user_name            => config.smtp_user,
+                  :password             => config.smtp_pass,
+                  :authentication       => 'plain',
+                  :enable_starttls_auto => true  }
+        delivery_method :smtp, options
+      end
+    end
     recipients.flatten!
     recipients.compact!
     recipients.uniq!
     return if(recipients.empty?)
     mail = ::Mail.deliver do
       content_type("text/plain; charset'utf-8'")
-      from(XmlConv::CONFIG.mail_from || 'dummy@test.com')
+      from(config.mail_from || 'dummy@test.com')
       to recipients
       subject = my_subject
       body = my_body
