04d661066d679a79c86b81d17702db8a2b33aac3
Niklaus Giger <niklaus.giger@member.fsf.org>
2016-06-22 10:30:48 +0200

    Commented out debug message which was emitted every minute

--- lib/xmlconv/util/polling_manager.rb
+++ lib/xmlconv/util/polling_manager.rb
@@ -66,7 +66,7 @@ module XmlConv
     class PopMission < Mission
       attr_accessor :host, :port, :user, :pass, :content_type
       def poll(&block)
-        puts "PopMission starts polling host #{@host}:#{@port} u: #{@user} pw: #{@pass}"
+        # puts "PopMission starts polling host #{@host}:#{@port} u: #{@user} pw: #{@pass}"
         options = {
                           :address    => @host,
                           :port       => @port,
