90c4c697194dffb2fb339233eece83ca3d115cf7
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2016-07-13 19:15:55 +0200

    Fix base_url issue as non flavored
    * Omit flavor from base_url
    * Add simple unit test for custom lookandfeel

---
+++ test/test_custom/lookandfeel.rb
@@ -0,0 +1,30 @@
+$: << File.dirname(__FILE__)
+$: << File.expand_path('..', File.dirname(__FILE__))
+$: << File.expand_path('../../lib', File.dirname(__FILE__))
+
+require 'minitest/autorun'
+require 'flexmock/minitest'
+require 'xmlconv/util/application'
+require 'xmlconv/util/session'
+require 'xmlconv/custom/lookandfeel'
+
+module XmlConv
+  class TestApplication < Util::Application
+    def unknown_user; end
+  end
+
+  module Custom
+    class TestLookandfeel < ::Minitest::Test
+      def setup
+        @app     = TestApplication.new
+        @session = Util::Session.new('test', @app)
+      end
+
+      def test_base_url_does_not_include_flavor
+        lookandfeel = Lookandfeel.new(@session)
+        assert_equal('sbsm', lookandfeel.flavor)
+        refute_match(lookandfeel.base_url, @session.flavor)
+      end
+    end
+  end
+end
