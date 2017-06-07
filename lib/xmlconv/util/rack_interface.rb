09ab99c7f5c7ea4716fb21b4a31db3e21b284e1c
Niklaus Giger <niklaus.giger@member.fsf.org>
2017-06-07 12:10:27 +0200

    Add forgotten rack_interface.rb

---
+++ lib/xmlconv/util/rack_interface.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'date'
+require 'sbsm/app'
+
+module XmlConv
+  module Util
+    class RackInterface < SBSM::RackInterface
+      ENABLE_ADMIN = true
+      SESSION = XmlConv::Util::Session
+      VALIDATOR = XmlConv::Util::Validator
+      def initialize(app: XmlConvApp,
+                     auth: nil,
+                     validator: XmlConv::Util::Validator)
+        [ File.join(Dir.pwd, 'etc', 'config.yml'),
+        ].each do |config_file|
+          if File.exist?(config_file)
+            SBSM.info "XmlConv.config.load from #{config_file}"
+            XmlConv.config.load (config_file)
+            break
+          end
+        end
+        @app = app
+        super(app: app,
+              session_class: XmlConv::Util::Session,
+              unknown_user: XmlConv::Util::KnownUser,
+              validator: validator,
+              cookie_name: 'virbac.bbmb'
+              )
+      end
+    end
+  end
+end
